c0b03596d06c677269534b77ca9d3d8d3809dd3b
ruicorte <ruicorte@gmail.com>
2017-01-04 17:56:16 +0000

    first daily commit

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
-var express = require('express')
-var server = require('http')
+"use strict";
 
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+
+var users = []
+var connections = []
+
+server.listen(3000, function(){
+	console.log('server listening on port 3000')
+})
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname+'/index.html')
+})
+
+io.sockets.on('connection', function(socket){
+	connections.push(socket)
+	console.log(`connected: ${connections.length} sockets connected.`)
+	// disconnect
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket), 1)
+		console.log(`disconnected: ${connections.length} sockets connected.`)
+	})
+	// send message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message',{msg:data,soc:connections.indexOf(socket)})
+	})
+})
