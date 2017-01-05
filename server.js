269e77af02457e5bf3a1952e10ea38d9acacba0a
ruicorte <ruicorte@gmail.com>
2017-01-05 15:03:37 +0000

    all done - simple chat app with usernames

--- server.js
+++ server.js
@@ -19,13 +19,31 @@ app.get('/', function(req, res){
 io.sockets.on('connection', function(socket){
 	connections.push(socket)
 	console.log(`connected: ${connections.length} sockets connected.`)
+
 	// disconnect
 	socket.on('disconnect', function(data){
+		users.splice(users.indexOf(socket.username),1)
+		updateUsernames()
 		connections.splice(connections.indexOf(socket), 1)
 		console.log(`disconnected: ${connections.length} sockets connected.`)
 	})
+
 	// send message
 	socket.on('send message', function(data){
-		io.sockets.emit('new message',{msg:data,soc:connections.indexOf(socket)})
+		io.sockets.emit('new message',{msg:data,user:socket.username})
 	})
+	
+	// new user
+	socket.on('new user',function(data, callback){
+		callback(true)
+		console.log(`nome de utilizador recebido: ${data}`)
+		socket.username = data
+		users.push(socket.username)
+		updateUsernames()
+	})
+
+	// send user names list to sockets
+	function updateUsernames(){
+		io.sockets.emit('get users', users)
+	}
 })
